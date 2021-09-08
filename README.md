8d44d2c6db216f59d9538c5b4fb5f9187c6faa9b
Huan He <hehuan2112@gmail.com>
2021-09-08 12:10:49 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# IEEEICHI2022
-Homepage for IEEE ICHI 2022
+# IEEE ICHI 2022
+
+Homepage for IEEE ICHI 2022.
+
+For development, run:
+
+```bash
+python server.py
+```
+
+For update the static version, run:
+
+```bash
+python server.py build
+```
+
+The updated static version will be saved into `docs` folder.
+
+Then, you could push the updates to GitHub to update the public site.
---
+++ server.py
@@ -0,0 +1,71 @@
+import sys
+from flask import Flask
+from flask import render_template
+from flask_frozen import Freezer
+
+FREEZER_DESTINATION = 'docs'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+freezer = Freezer(app)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/call_for_contributions.html")
+def call_for_contributions():
+    return render_template('call_for_contributions.html')
+
+
+@app.route("/call_for_papers.html")
+def call_for_papers():
+    return render_template('call_for_papers.html')
+
+
+@app.route("/call_for_posters_and_demos.html")
+def call_for_posters_and_demos():
+    return render_template('call_for_posters_and_demos.html')
+
+
+@app.route("/call_for_workshops.html")
+def call_for_workshops():
+    return render_template('call_for_workshops.html')
+
+
+@app.route("/call_for_doctorial_consortium.html")
+def call_for_doctorial_consortium():
+    return render_template('call_for_doctorial_consortium.html')
+
+
+@app.route("/call_for_industry_track.html")
+def call_for_industry_track():
+    return render_template('call_for_industry_track.html')
+
+
+@app.route("/call_for_tutorials.html")
+def call_for_tutorials():
+    return render_template('call_for_tutorials.html')
+
+
+@app.route("/program.html")
+def program():
+    return render_template('program.html')
+
+
+@app.route("/keynotes.html")
+def keynotes():
+    return render_template('keynotes.html')
+
+
+@app.route("/committees.html")
+def committees():
+    return render_template('committees.html')
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        freezer.freeze()
+    else:
+        app.run(debug=True)
